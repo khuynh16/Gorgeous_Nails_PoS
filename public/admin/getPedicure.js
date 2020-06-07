67f20dcda323d5816094d98a762f0b999da620f8
kh123 <kelvinahuynh@gmail.com>
2020-06-07 00:36:03 -0400

    finished front end of admin add service page

--- public/admin/getPedicure.js
+++ public/admin/getPedicure.js
@@ -1,21 +1,20 @@
-async function getNails() {
-    return await fetch('http://localhost:3000/nails')
+async function getPedicure() {
+    return await fetch('http://localhost:3000/pedicure')
                         .then((response) => response.json())
                         .then((data) => data);
 }
 
 document.addEventListener('DOMContentLoaded', async function() {
 
-    //NOTE: this section is to fill out nails tab on remove employees admin page section
-    let nails = await getNails();
-    let nails_tab = document.querySelector('.currentNails');
+    //NOTE: this section is to fill out pedicure tab on add services admin page section
+    let pedicure = await getPedicure();
+    let pedicure_tab = document.querySelector('.currentPedicure');
 
-    nails.forEach((service) => {
+    pedicure.forEach((service) => {
         let serviceHTML =   `<li class="list-group-item d-flex justify-content-between lh-condensed">
                                 <h6 class="my-0 px-0 col-9">${service.name}</h6>
                                 <span class="text-muted col-2 pl-0 pCost">$${service.cost.toFixed(2)}</span>
                             </li>`;
-        nails_tab.insertAdjacentHTML('beforeend', serviceHTML);
+        pedicure_tab.insertAdjacentHTML('beforeend', serviceHTML);
     })
-
 })
